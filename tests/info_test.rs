ce6a660e1ed3df0faba9c5b96c03b0ec68f069da
Eric <241420+Glitchvid@users.noreply.github.com>
2021-09-13 17:38:57 -0600

    Add test case for A2S_INFO servers Add warning about failing tests due to conditions instead of actual code problems.

--- tests/info_test.rs
+++ tests/info_test.rs
@@ -1,3 +1,7 @@
+// Any tests connecting to a server may fail in the future,
+// either due to intermittent internet connection to the IP(s),
+// or from the IP space being re-allocated to non-SRCDS endpoints.
+
 #[cfg(not(feature = "async"))]
 #[test]
 fn test_info() {
@@ -5,5 +9,21 @@ fn test_info() {
 
     let result = client.info("play.maxdb.net:27015").unwrap();
 
+    println!("{:?}", result);
+}
+
+/// As of 2020-12-07 servers may opt-in to a **S2C_CHALLENGE** response for the
+/// **A2S_INFO** request.
+///
+/// So this test covers specifically cases where we expect an S2C response.
+///
+/// See Announcement: 
+/// https://steamcommunity.com/discussions/forum/14/2974028351344359625/
+#[cfg(not(feature = "async"))]
+#[test]
+fn test_info_challenge() {
+    let valve_lax2_l4d2 = "162.254.195.121:27083"; // srcds1157-lax2.115.69
+    let client = a2s::A2SClient::new().unwrap();
+    let result = client.info(valve_lax2_l4d2).unwrap();
     println!("{:?}", result);
 }
